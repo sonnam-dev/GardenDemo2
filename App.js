ebfb9a678803d61fb9c121d0ce55f33208cb7275
DWMT_SN <61965110+sonnam-dev@users.noreply.github.com>
2021-03-24 14:21:03 +0700

    database1

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,56 +6,37 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SignUp from './components/LoginForder/SignUp';
 import Login from './components/LoginForder/Login';
 import Main from './components/LoginForder/main';
+import database from '@react-native-firebase/database';
 
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator >
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Main" component={Main} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
-// import React, { useState, useEffect } from 'react';
-// import { View, Text } from 'react-native';
-// import auth from '@react-native-firebase/auth';
+const reference = database().ref('/users/123456');
+// const Stack = createStackNavigator();
 
 // function App() {
-//   // Set an initializing state whilst Firebase connects
-//   const [initializing, setInitializing] = useState(true);
-//   const [user, setUser] = useState();
-
-//   // Handle user state changes
-//   function onAuthStateChanged(user) {
-//     setUser(user);
-//     if (initializing) setInitializing(false);
-//   }
-
-//   useEffect(() => {
-//     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-//     return subscriber; // unsubscribe on unmount
-//   }, []);
-
-//   if (initializing) return null;
-
-//   if (!user) {
-//     return (
-//       <View>
-//         <Text>Login</Text>
-//       </View>
-//     );
-//   }
-
 //   return (
-//     <View>
-//       <Text>Welcome {user.email}</Text>
-//     </View>
+//     <NavigationContainer>
+//       <Stack.Navigator >
+//         <Stack.Screen name="SignUp" component={SignUp} />
+//         <Stack.Screen name="Login" component={Login} />
+//         <Stack.Screen name="Main" component={Main} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
 //   );
 // }
+
 // export default App;
+function User({ userId }) {
+  useEffect(() => {
+    const onValueChange = database()
+      .ref('/users/${userId}')
+      .on('value', snapshot => {
+        console.log('User data: ', snapshot.val());
+      });
+
+    // Stop listening for updates when no longer required
+  //   return () =>
+  //     database()
+  //       .ref('/users/${userId}')
+  //       .off('value', onValueChange);
+  }, [userId]);
+}
+ export default User;
