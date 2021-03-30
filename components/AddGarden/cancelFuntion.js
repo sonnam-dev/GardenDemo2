fa4f4b53b2dfd53ca04330be2ac49ccc8f31d3d8
trungkay <trungtnhe141295@fpt.edu.vn>
2021-03-31 00:21:23 +0700

    cancelFuntion

---
+++ components/AddGarden/cancelFuntion.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import { Button, View, Text,TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import styles from '../style/styles';
+ 
+
+export default function cancelFuntion(){
+    return(
+        <View style={{flex: 1,backgroundColor: 'black'}}>
+            <View style={styles.viewTypetext2} >
+            <Text>crete new enter DeleteButton</Text>
+            <Text>hi</Text>
+            </View>
+        </View>
+    )
+}
